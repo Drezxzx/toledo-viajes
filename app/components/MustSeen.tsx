0e239b4ac59f2c61917b0aff91845b24df03a38d
Andres <andres250197@hotmail.com>
2024-12-04 17:54:33 +0100

    0.3

--- app/components/MustSeen.tsx
+++ app/components/MustSeen.tsx
@@ -3,28 +3,28 @@ export default function MustSeen() {
     const atractions = [
         {
             title: "La Catedral de Toledo",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatem.",
+            description: "La catedral de Santa María, llamada también catedral primada de España, es un templo católico de arquitectura gótica en Toledo, considerado por algunos como el opus magnum1​ de este estilo en España. Su construcción comenzó en 1226 bajo el reinado de Fernando III el Santo y las últimas aportaciones góticas se dieron en siglo XV cuando en 1493 se cerraron las bóvedas de los pies de la nave central, en tiempos de los Reyes Católicos.",
             image: "./test.jpg"
         },
         {
-            title: "La Catedral de Toledo",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatem.",
-            image: "./test.jpg"
+            title: "Alcázar de Toledo",
+            description: "Encaramado en la cima de la colina en la ciudad de Toledo, España, el Alcázar de Toledo es un palacio fortificado con paredes de granito. El edificio se distingue por cuatro torres cuadrangulares, concentradas en torno a una amplia explanada, un palacio visigodo y una fortaleza. Las fachadas tienen un estilo arquitectónico renacentista.",
+            image: "./alcazar.webp"
         },        
         {
-            title: "La Catedral de Toledo",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatem.",
-            image: "./test.jpg" 
+            title: "la Judería",
+            description: "El barrio de la judería fue uno de los más importantes y destacados de Castilla. De hecho, historiadores como Julio Porres sostienen que llegó a haber dos juderías: la Judería Mayor (lo que hoy se entiende como el barrio de la judería y que se visita) y la Judería Menor que estuvo ubicada muy próxima a la Catedral de Toledo.",
+            image: "./ronda.jpeg" 
         },        
         {
-            title: "La Catedral de Toledo",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatem.",
-            image: "./test.jpg" 
+            title: "San Juan de los Reyes",
+            description: "El convento de San Juan de los Reyes es un cenobio de la ciudad española de Toledo perteneciente a la Orden Franciscana, que fue construido bajo el patrocinio de la reina Isabel I de Castilla con la intención de convertirlo en mausoleo real, en conmemoración de la batalla de Toro y del nacimiento del príncipe Juan. Se trata de una de las más valiosas muestras del estilo gótico isabelino y el edificio más importante erigido por los Reyes Católicos.",
+            image: "./san-juan.png" 
         },        
         {
-            title: "La Catedral de Toledo",
-            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, voluptatem.",
-            image: "./test.jpg" 
+            title: "Museo de El Greco",
+            description: 'En su interior se encuentra parte de la obra de El Greco, de la que destacan un magnífico "apostolado", la "Vista y plano de Toledo" y "Las lágrimas de San Pedro". El museo se completa con cuadros de Luis Tristán, Murillo, Valdés Leal y otros artistas',
+            image: "./museogreco.jpg" 
         }
     ]
 
@@ -35,9 +35,8 @@ export default function MustSeen() {
                 {
                     atractions.map(({image, title, description}, i) =>(
                         <div className="hover:scale-105 transition-all cursor-pointer" key={i}>
-                            <img src={image} alt={title} className="rounded-xl object-cover"/>
+                            <img src={image} alt={title} className="rounded-xl object-cover w-72 h-[7rem]"/>
                             <h3 className="font-medium text-base">{title}</h3>
-                            <p className="text-sm text-parrafos">{description}</p>
                         </div>
                     ))
                 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import MustSeen from "./components/MustSeen";
 
 export default function Home() {
   return (
-    <main className="flex h-svh flex-col gap-1">
+    <main className="flex w-screen h-svh flex-col gap-1">
       <Header/>
       <section className="lg:max-w-[950px] flex flex-col h-full gap-5 lg:p-0 p-2 mx-auto mb-2">
         <DiscoverHero/>
