a7cb4338c009c68d323a9602ac3416e82f039073
Andres <andres250197@hotmail.com>
2024-12-04 17:56:46 +0100

    change margin top

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="w-screen flex justify-between items-center p-2 border-b border-borderColor">
+        <header className="w-screen flex justify-between items-center px-2 py-1 border-b border-borderColor">
             <div>
                 <h1 className="text-xl font-bold">Toledo viajes</h1>
             </div>
