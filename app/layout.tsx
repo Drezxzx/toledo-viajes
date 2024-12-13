c3aa45feb795759ffc5c160e15d310e062e808f4
Andres <andres250197@hotmail.com>
2024-12-13 22:01:46 +0100

    add analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="es">
       <body
       >
+        <Analytics />
         <SessionProvider>
           <Header />
         {children}
