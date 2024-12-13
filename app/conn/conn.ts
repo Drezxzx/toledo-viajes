d364b265cb552026fbcc0ef572015d064a32ad9e
Andres <andres250197@hotmail.com>
2024-12-13 22:00:02 +0100

    add conn

--- app/conn/conn.ts
+++ app/conn/conn.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@libsql/client";
 
-const client = createClient({
-  url: "http://127.0.0.1:8080",
-});
-
 // const client = createClient({
-//     url: process.env.TURSO_DATABASE_URL,
-//     authToken: process.env.TURSO_AUTH_TOKEN,
-//   });
+//   url: "http://127.0.0.1:8080",
+// });
+
+const client = createClient({
+    url: process.env.TURSO_DATABASE_URL as string,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  });
 export default client;
